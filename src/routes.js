663da2d903216ca0fba96de5b707c3d0e4484ef5
MPansur1 <mayuri.pansuriya1@t-mobile.com>
2020-02-17 12:25:59 +0530

    changes in route

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import Router from "vue-router";
 const Bar = { template: "<div>bar</div>" };
 const Foo = { template: "<div>foo</div>" };
 import ClientDashBoard from "./views/client/Dashboard";
+
+import { getToken } from "./utils/auth"; // get token from cookie
+import store from "./store";
 Vue.use(Router);
 
 export function createRouter() {
-  return new Router({
+  const router = new Router({
     mode: "history",
     routes: [
       {
@@ -28,4 +31,85 @@ export function createRouter() {
       }
     ]
   });
+
+  router.beforeEach((to, from, next) => {
+    console.info("before route ::", to.path);
+    // determine whether the user has logged in
+    const hasToken = getToken();
+    if (to.path === "/login") {
+      next({ path: "/" });
+    } else if (
+      to.matched.some(record => record.meta.requiresAuth) &&
+      !hasToken
+    ) {
+      console.info("route has auth but token not found");
+      next({ path: "/" });
+    } else if (to.matched.some(record => record.meta.requiresAuth)) {
+      console.info("route has auth", to.path);
+      if (to.path === "/login") {
+        // if is logged in, redirect to the home page
+        next({ path: "/" });
+      } else {
+        const hasGetUserInfo = store.getters.name;
+        if (hasGetUserInfo) {
+          const hasUserRole = store.getters.role;
+          if (to.matched.some(record => record.meta.adminAuth)) {
+            if (hasUserRole && hasUserRole === "admin") {
+              next();
+            } else {
+              next({ path: "/" });
+            }
+          } else {
+            next();
+          }
+        } else {
+          try {
+            console.info("authenticate token");
+            // get user info
+            store.dispatch("user/getInfo");
+            if (to.matched.some(record => record.meta.adminAuth)) {
+              const hasUserRole = store.getters.role;
+              if (hasUserRole && hasUserRole === "admin") {
+                next();
+              } else {
+                next({ path: "/" });
+              }
+            } else {
+              next();
+            }
+            // next()
+          } catch (error) {
+            // remove token and go to login page to re-login
+            store.dispatch("user/resetToken");
+            next(`/`);
+          }
+        }
+      }
+    } else if (hasToken && to.path == "/") {
+      try {
+        console.info("has token");
+        // get user info
+        store.dispatch("user/getInfo");
+        if (to.matched.some(record => record.meta.adminAuth)) {
+          const hasUserRole = store.getters.role;
+          if (hasUserRole && hasUserRole === "admin") {
+            next();
+          } else {
+            next({ path: "/" });
+          }
+        } else {
+          next();
+        }
+      } catch (error) {
+        // remove token and go to login page to re-login
+        store.dispatch("user/resetToken");
+        next(`/`);
+      }
+    } else {
+      console.info("next ::", to.path);
+      next();
+    }
+  });
+
+  return router;
 }
